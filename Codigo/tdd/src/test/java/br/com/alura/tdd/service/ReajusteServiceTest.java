9d91bfe93c0aff256293f7892b1dbf479562f576
klayton-a-souza <klayton.al.souza@gmail.com>
2022-11-07 13:49:58 -0300

    Codigo 5.2: Organizando o código de teste

--- Codigo/tdd/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ Codigo/tdd/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -5,41 +5,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
-	
+
+	private ReajusteService service;
+	private Funcionario funcionario;
+
+	@BeforeEach
+	public void inicializando() {
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario("Klayton", LocalDate.now(), new BigDecimal("1000.00"));
+	}
+
 	@Test
-	public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Klayton",LocalDate.now(), new BigDecimal("1000.00"));
-		
+	public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar() {		
 		service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
-		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
-	
+
 	@Test
 	public void reajusteDeveriaSerDeQuinzePorcentoQuandoDesempenhoForBom() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Klayton",LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.BOM);
-		
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
-	
+
 	@Test
 	public void reajusteDeveriaSerDeVintePorcentoQuandoDesempenhoForOtimo() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Klayton",LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.OTIMO);
-		
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
-	
+
 }
